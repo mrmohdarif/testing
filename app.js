4b6d247327d0e8240e89ad0187ffdb6599e5b978
MOHD ARIF <122776196+mrmohdarif@users.noreply.github.com>
2025-08-05 15:23:19 +0530

    Update app.xc
    sdsds

--- app.js
+++ app.js
@@ -34,10 +34,10 @@ let questionIndex = 0;
 // });
 
 app.get('/', (req, res) => {
- res.send("Hello, please introduce yourself.");
+ res.send("Hello, please introduce yourself.vcvcvcvcv");
 })
 app.get('/testing', (req, res) => {
- res.send("How do you handle project deadlines under pressure?");
+ res.send("How do you handle project deadlines under pressure?vcvcvcvcvcdf");
 })
 
 
